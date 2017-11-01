ea86adfd6d1ec01a1bfbe6db20b3bd913b0e7857
Mohit Reddy <mohitreddy1996@gmail.com>
2017-11-02 04:06:55 +0530

    Features generator done.

---
+++ data_processor/feature_extractor.py
@@ -0,0 +1,51 @@
+import os
+from PIL import Image
+import cv2
+import logging
+
+from utils.utils import DATA_SET_TRAIN_POSITIVE_, DATA_SET_TRAIN_NEGATIVE_, FEATURES_LOCATION, save_features_in_csv, \
+    get_largest_contour, get_contour_arch_length, get_contour_area, get_eccentricity, get_contour_moments
+
+
+def get_features_from_image(image):
+    """
+    Return features of the largest contour (Assuming it will be the bacteria in this image or background.
+    :param image: Input image.
+    :return: list of features.
+    """
+    largest_contour = get_largest_contour(image)
+    contour_area = get_contour_area(largest_contour)
+    contour_arch_length = get_contour_arch_length(largest_contour)
+    compactness = contour_arch_length / contour_area
+    eccentricity = get_eccentricity(largest_contour)
+    moments = get_contour_moments(largest_contour)
+    features = {"moments": moments, "compactness": compactness, "eccentricity": eccentricity}
+    return features
+
+
+def extract_features():
+    label = 0  # negative image.
+    ans = 0
+    for file_name in os.listdir(DATA_SET_TRAIN_NEGATIVE_):
+        if file_name.endswith(".png"):
+            image = cv2.imread(DATA_SET_TRAIN_NEGATIVE_ + file_name)
+            try:
+                features_list = get_features_from_image(image)
+                save_features_in_csv(features_list, label, FEATURES_LOCATION)
+                ans += 1
+            except:
+                logging.debug("Error getting features for " + file_name)
+    label = 1  # positive images.
+    for file_name in os.listdir(DATA_SET_TRAIN_POSITIVE_):
+        if file_name.endswith(".png"):
+            image = cv2.imread(DATA_SET_TRAIN_POSITIVE_ + file_name)
+            try:
+                features_list = get_features_from_image(image)
+                save_features_in_csv(features_list, label, FEATURES_LOCATION)
+                ans += 1
+            except:
+                logging.debug("Error getting features for " + file_name)
+    print ans
+
+if __name__ == '__main__':
+    extract_features()
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,7 @@
+import cv2
+import csv
+import logging
+
 DATA_SET_DIR = "/home/mohit/Classification-of-Mycobaterial-tuberculosis/datasets/"
 
 DATA_SET_TRAIN = DATA_SET_DIR + "train/"
@@ -5,6 +9,70 @@ DATA_SET_TRAIN = DATA_SET_DIR + "train/"
 DATA_SET_TRAIN_POSITIVE = DATA_SET_TRAIN + "batch_positive/"
 DATA_SET_TRAIN_NEGATIVE = DATA_SET_TRAIN + "batch_negative/"
 
+DATA_SET_TRAIN_POSITIVE_ = DATA_SET_TRAIN + "positive/"
+DATA_SET_TRAIN_NEGATIVE_ = DATA_SET_TRAIN + "negative/"
+
 PIXEL_CLASSIFIER_TEST_INPUT = DATA_SET_DIR + "test/"
 
 TEST_FILE_1 = "test1.png"
+
+FEATURES_LOCATION = "/home/mohit/Classification-of-Mycobaterial-tuberculosis/features.csv"
+FEATURES_ORDER = ["compactness", "eccentricity", "nu11", "nu12", "nu21", "nu02", "nu20", "nu03", "nu30"]
+
+
+def save_features_in_csv(features_list, label, csv_file_loc):
+    features = []
+    for feature in FEATURES_ORDER:
+        if feature in features_list:
+            features.append(features_list[feature])
+        else:
+            features.append(features_list["moments"][feature])
+    features.append(label)
+    with open(csv_file_loc, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(features)
+        f.close()
+    logging.info("Successfully written to csv.")
+
+
+def get_largest_contour(image):
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(image_gray, 150, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    max_area = 0
+    max_index = -1
+    for contour_index in range(len(contours)):
+        contour_area = cv2.contourArea(contours[contour_index])
+        if contour_area > max_area and len(contours) >= 5:
+            max_area = contour_area
+            max_index = contour_index
+    if max_index == -1 and len(contours) > 0:
+        """Todo: hack!!"""
+        max_contour = contours[0]
+    else:
+        max_contour = contours[max_index]
+    return max_contour
+
+
+def get_contour_area(contour):
+    return cv2.contourArea(contour=contour)
+
+
+def get_contour_arch_length(contour):
+    return cv2.arcLength(contour, closed=True)
+
+
+def get_eccentricity(contour):
+    ellipse = cv2.fitEllipse(contour)
+    b = ellipse[1][0]
+    a = ellipse[1][1]
+    return (1 - (b**2/a**2))**0.5
+
+
+def get_contour_moments(contour):
+    moments = cv2.moments(contour)
+    norm_moments = {}
+    for key in moments:
+        if 'nu' in key:
+            norm_moments[key] = moments[key]
+    return norm_moments
